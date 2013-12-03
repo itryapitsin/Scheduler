b59df7290f7ca849f459e51340537499f03f1c17
IliyaTryapitsin <iliya.tryapitsin@gmail.com>
2013-12-03 15:56:07 +0400

    build fixing

--- Host/Timetable.Site/Areas/Dispatcher/Controllers/LecturerScheduleController.cs
+++ Host/Timetable.Site/Areas/Dispatcher/Controllers/LecturerScheduleController.cs
@@ -15,8 +15,6 @@ namespace Timetable.Site.Areas.Dispatcher.Controllers
     {
         public PartialViewResult Index()
         {
-            var sid = UserData.LecturerScheduleSettings.StudyYearId;
-
             var model = new LecturerScheduleViewModel
             {
                 StudyYears = DataService
@@ -28,76 +26,25 @@ namespace Timetable.Site.Areas.Dispatcher.Controllers
                     .Select(x => new SemesterViewModel(x)),
 
                 StudyYearId = UserData.LecturerScheduleSettings.StudyYearId,
+
                 Semester = UserData.LecturerScheduleSettings.SemesterId,
-                LecturerSearchString = UserData.LecturerScheduleSettings.LecturerId.HasValue ?
-                                       new LecturerViewModel(DataService.GetLecturerById(UserData.LecturerScheduleSettings.LecturerId.Value)).Name
-                                       : null
+
+                LecturerSearchString = UserData.LecturerScheduleSettings.LecturerId.HasValue 
+                    ? new LecturerViewModel(DataService.GetLecturerById(UserData.LecturerScheduleSettings.LecturerId.Value)).Name
+                    : null
             };
 
             return PartialView("_Index", model);
         }
 
-        //    var model = new LecturerScheduleViewModel
-        //    {
-        //        StudyYears = DataService
-        //            .GetStudyYears()
-        //            .Select(x => new StudyYearViewModel(x)),
+        public ActionResult SearchLecturer(string query)
+        {
+            var lecturers = DataService
+                .GetLecturersByFirstMiddleLastname(query)
+                .ToEnumerableWithTotal(x => (new LecturerViewModel(x).Name));
 
-        //        Semesters = DataService
-        //            .GetSemesters()
-        //            .Select(x => new SemesterViewModel(x)),
-
-        //        StudyYearId = UserData.LecturerScheduleSettings.StudyYearId,
-        //        Semester = UserData.LecturerScheduleSettings.SemesterId,
-        //        LecturerSearchString = UserData.LecturerScheduleSettings.LecturerId.HasValue ?
-        //                               new LecturerViewModel(DataService.GetLecturerById(UserData.LecturerScheduleSettings.LecturerId.Value)).Name
-        //                               : null
-        //    };
-
-        //    return PartialView("_Index", model);
-        //}
-
-        //public ActionResult SearchLecturer(string query)
-        //{
-        //    var lecturers = DataService
-        //        .GetLecturersByFirstMiddleLastname(query)
-        //        .ToEnumerableWithTotal(x => (new LecturerViewModel(x).Name));
-
-        //    return new JsonNetResult(lecturers);
-        //}
-
-        //public ActionResult LoadLecturerSchedule(LecturerScheduleRequest request)
-        //{
-
-        //    UserData.LecturerScheduleSettings.SemesterId = request.Semester;
-        //    UserData.LecturerScheduleSettings.StudyYearId = request.StudyYearId;
-        //    UserService.SaveUserState(UserData);
-
-        //    var currentLecturer = DataService.GetLecturerBySearchQuery(request.LecturerQuery);
-
-        //    var schedules = new List<ScheduleViewModel>();
-        //    var times = new List<TimeViewModel>();
-
-        //    if (currentLecturer != null)
-        //    {
-        //        UserData.LecturerScheduleSettings.LecturerId = currentLecturer.Id;
-        //        UserService.SaveUserState(UserData);
-
-        //        schedules = DataService.GetSchedulesForLecturer(currentLecturer.Id, request.StudyYearId, request.Semester)
-        //            .ToList()
-        //            .Select(x => new ScheduleViewModel(x)).ToList();
-
-        //        var timeIds = schedules.Select(x => x.TimeId).Distinct().ToList();
-
-        //        times = DataService.GetTimesByIds(timeIds).ToList().Select(x => new TimeViewModel(x)).ToList();
-        //    }
-
-        //    return new JsonNetResult(new ScheduleForLecturersWithTimesViewModel()
-        //    {
-        //        Schedules = schedules,
-        //        Times = times
-        //    });
-        //}
+            return new JsonNetResult(lecturers);
+        }
 
         public ActionResult LoadLecturerSchedule(LecturerScheduleRequest request)
         {
