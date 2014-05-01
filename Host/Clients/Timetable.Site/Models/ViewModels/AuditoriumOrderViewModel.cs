044e3a42f911f402ad57b51ce239c26771eabbf4
dmpyatin <dmpyatin@gmail.com>
2014-05-02 00:37:38 +0400

    Add AuditoriumOrder and Exam ViewModels

---
+++ Host/Clients/Timetable.Site/Models/ViewModels/AuditoriumOrderViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Timetable.Logic.Models.Scheduler;
+
+
+namespace Timetable.Site.Models.ViewModels
+{
+    public class AuditoriumOrderViewModel
+    {
+        public int Id { get; set; }
+
+        public string ThreadName { get; set; }
+
+        public string LecturerName { get; set; }
+
+        public string TutorialName { get; set; }
+
+        public int DayOfWeek { get; set; }
+
+        public TimeViewModel Time { get; set; }
+
+        public int TimeId { get; set; }
+
+        public int Pair { get; set; }
+
+        //public AuditoriumViewModel Auditorium { get; set; }
+
+        public string AuditoriumNumber { get; set; }
+
+        public int AuditoriumId { get; set; }
+
+        public bool AutoDelete { get; set; }
+
+        public AuditoriumOrderViewModel(AuditoriumOrderDataTransfer auditoriumOrder)
+        {
+            Id = auditoriumOrder.Id;
+
+            ThreadName = auditoriumOrder.ThreadName;
+
+            LecturerName = auditoriumOrder.LecturerName;
+
+            TutorialName = auditoriumOrder.TutorialName;
+
+            DayOfWeek = auditoriumOrder.DayOfWeek;
+
+            Time = new TimeViewModel(auditoriumOrder.Time);
+
+            if (auditoriumOrder.Time != null) 
+            {
+                TimeId = auditoriumOrder.Time.Id;
+                Pair = auditoriumOrder.Time.Position;
+            }
+
+            //Auditorium = new AuditoriumViewModel(auditoriumOrder.Auditorium);
+
+            if (auditoriumOrder.Auditorium != null)
+            {
+                AuditoriumNumber = auditoriumOrder.Auditorium.Number;
+                AuditoriumId = auditoriumOrder.Auditorium.Id;
+            }
+
+            
+        }
+    }
+}
---
+++ Host/Clients/Timetable.Site/Models/ViewModels/ExamViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Timetable.Logic.Models.Scheduler;
+
+namespace Timetable.Site.Models.ViewModels
+{
+    public class ExamViewModel
+    {
+        public int Id { get; set; }
+
+        public string LecturerName { get; set; }
+
+        public int LecturerId { get; set; }
+
+        public string GroupCode { get; set; }
+
+        public int GroupId { get; set; }
+
+        public string TutorialName { get; set; }
+
+        public int TutorialId { get; set; }
+
+        public string AuditoriumNumber { get; set; }
+
+        public int AuditoriumId { get; set; }
+
+        public string Time { get; set; }
+
+        public ExamViewModel(ExamDataTransfer exam)
+        {
+            Id = exam.Id;
+            if (exam.Lecturer != null)
+            {
+                LecturerName = LecturerViewModel.GetLecturerShortName(exam.Lecturer);
+                LecturerId = exam.Lecturer.Id;
+            }
+
+            if (exam.Group != null)
+            {
+                GroupCode = exam.Group.Code;
+                GroupId = exam.Group.Id;
+            }
+
+            if (exam.Tutorial != null)
+            {
+                TutorialName = exam.Tutorial.Name;
+                TutorialId = exam.Tutorial.Id;
+            }
+
+            if (exam.Auditorium != null)
+            {
+                AuditoriumNumber = exam.Auditorium.Number;
+                AuditoriumId = exam.Auditorium.Id;
+            }
+
+            Time = exam.Time.ToString("MM-dd-yyyy-hh:mm");
+        }
+    }
+}
